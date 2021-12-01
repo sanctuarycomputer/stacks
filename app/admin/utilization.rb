436157d435dbfa6d2e19174bbdd8c3cea0d353ae
Hugh Francis <hugh@sanctuary.computer>
2021-12-01 10:49:00 -0500

    Update Utilization to include hours sold

--- app/admin/utilization.rb
+++ app/admin/utilization.rb
@@ -1,19 +1,8 @@
 ActiveAdmin.register_page "Utilization" do
-  menu if: proc { current_admin_user.email == "hugh@sanctuary.computer" },
-       priority: 2
-
   content title: "Utilization" do
     # What does a billable hour cost us, on average?
     utilization_pass = UtilizationPass.first
     profitability_pass = ProfitabilityPass.first
-
-    #{
-    #  "May, 2021": {
-    #    billable_hours,
-    #    cost_of_doing_business,
-    #    cost_per_hour
-    #  }
-    #}
 
     aggregated_data = utilization_pass.data.keys.reduce({}) do |acc, year|
       months = utilization_pass.data[year].keys.sort do |a, b|
@@ -51,8 +40,8 @@ ActiveAdmin.register_page "Utilization" do
           acc += u["allocation"]
         end
         acc[label]["average_hourly_rate"] = ((monthly_aggregate.reduce(0) do |acc, u|
-          acc += u["hourly_rate"]
-        end) / monthly_aggregate.length)
+          acc += (u["hourly_rate"] * u["allocation"])
+        end) / acc[label]["total_hours_sold"])
         acc[label]["cost_per_billable_hour"] =
           acc[label]["cogs"] / acc[label]["total_hours_sold"]
       end
@@ -66,11 +55,19 @@ ActiveAdmin.register_page "Utilization" do
       datasets: [{
         label: 'Internal Cost per Billable Hour',
         borderColor: COLORS[0],
-        data: aggregated_data.values.map{|v| v["cost_per_billable_hour"]}
+        data: aggregated_data.values.map{|v| v["cost_per_billable_hour"]},
+        yAxisID: 'y',
       }, {
         label: 'Average Hourly Rate Billed',
         borderColor: COLORS[1],
-        data: aggregated_data.values.map{|v| v["average_hourly_rate"]}
+        data: aggregated_data.values.map{|v| v["average_hourly_rate"]},
+        yAxisID: 'y',
+      }, {
+        label: 'Total Hours Sold',
+        borderColor: COLORS[2],
+        data: aggregated_data.values.map{|v| v["total_hours_sold"]},
+        yAxisID: 'y1',
+        type: 'bar'
       }]
     }
 
