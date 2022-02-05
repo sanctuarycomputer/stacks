6cf1cd0622fecc1f4ff667c4dab934b6a2ac54fe
Hugh Francis <hugh@sanctuary.computer>
2022-02-05 11:25:02 -0700

    Do matching on callbacks

--- app/models/expense_group.rb
+++ app/models/expense_group.rb
@@ -1,6 +1,13 @@
 class ExpenseGroup < ApplicationRecord
   has_many :qbo_purchase_line_items
 
+  after_save :do_matching
+  after_destroy :do_matching
+
+  def do_matching
+    Stacks::Expenses.match_all!
+  end
+
   def spent_last_month
     qbo_purchase_line_items.where(
       txn_date: (Date.today - 1.month).beginning_of_month...(Date.today - 1.month).end_of_month
