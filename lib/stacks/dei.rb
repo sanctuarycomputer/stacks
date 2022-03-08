41383583ca0fc6417c76041a509bb96589bc76e5
Hugh Francis <hugh@sanctuary.computer>
2022-03-08 09:27:54 -0500

    Fix issue with counting people who've not submitted DEI responses

--- lib/stacks/dei.rb
+++ lib/stacks/dei.rb
@@ -2,6 +2,10 @@ class Stacks::Dei
   class << self
     def make_rollup(force = false)
       return if DeiRollup.this_month.any? unless force
+      DeiRollup.this_month.delete_all
+
+      admin_users_with_dei_response =
+        AdminUser.active.select{|a| !a.should_nag_for_dei_data?}
 
       data = [
         RacialBackground,
@@ -9,13 +13,17 @@ class Stacks::Dei
         GenderIdentity,
         Community,
       ].reduce({
-        meta: { total: AdminUser.active.count }
+        meta: {
+          total: admin_users_with_dei_response.count
+        }
       }) do |acc, klass|
         join_klass = "AdminUser#{klass.to_s}".constantize
         acc[klass.to_s.underscore] = klass.all.map do |o|
           getter = {}
           getter[klass.to_s.underscore] = o
           joins = join_klass.preload(:admin_user).where(getter)
+          # Filter joins for Admin Users who haven't finished their DEI response
+          joins = joins.select{|j| admin_users_with_dei_response.include?(j.admin_user)}
           {
             id: o.id,
             name: o.name,
