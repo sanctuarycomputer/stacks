5cda385afb832f4c462a948137e12f81fd702a34
Hugh Francis <hugh@sanctuary.computer>
2022-10-30 18:52:21 -0400

    OK

--- app/models/social_property.rb
+++ app/models/social_property.rb
@@ -7,7 +7,6 @@ class SocialProperty < ApplicationRecord
     browser.headers.add({
       "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
       "Referer" => "https://www.google.com/",
-      "Cookie" => 'lang=v=2&lang=en-us; lidc="b=VGST06:s=V:r=V:a=V:p=V:g=2510:u=1:x=1:i=1667167195:t=1667253595:v=2:sig=AQE_kkroPVypqU1QrwkkfF6no9wxUBMH"; bcookie="v=2&7d795330-a473-497f-87a1-0db34e753f4f"; bscookie="v=1&20221030215954e8ff523c-f14e-41e7-8d82-24335bb00013AQGUYx9lilc1LdnHCTRUSGX_hdGLRR35'
     })
     browser.go_to(profile_url)
 
@@ -23,7 +22,7 @@ class SocialProperty < ApplicationRecord
       end
 
     return unless followers_el.present?
-    followers_count = followers_el.inner_text.gsub(/[,.]/,'').split(" ").find{|t| t.to_i > 0}.try(:to_i) || 0
+    followers_count = followers_el.inner_text.gsub(/[,.]/,'').split(" ").find{|t| (t.try(:to_i) || 0) > 0}.try(:to_i) || 0
     update!(snapshot: snapshot.merge({ DateTime.now.iso8601 => followers_count })) if followers_count > 0
   end
 end
