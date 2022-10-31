912b97c9a7b882190f6a89945d9c821381c5cca8
Hugh Francis <hugh@sanctuary.computer>
2022-10-31 09:23:47 -0400

    Tidy up sampling code

--- app/models/social_property.rb
+++ app/models/social_property.rb
@@ -8,7 +8,11 @@ class SocialProperty < ApplicationRecord
       "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
       "Referer" => "https://www.google.com/",
     })
+
+    puts "Navigating to #{profile_url}"
     browser.go_to(profile_url)
+    browser.network.wait_for_idle(duration: 0.5)
+    sleep(1.second)
 
     followers_el =
       if profile_url.include?("twitter.com")
@@ -21,8 +25,14 @@ class SocialProperty < ApplicationRecord
         browser.evaluate("Array.from(document.querySelectorAll('a')).find(b => b.innerText.includes('followers'));")
       end
 
-    return unless followers_el.present?
+    unless followers_el.present?
+      # binding.pry
+      # browser.screenshot(path: "screenshot.png")
+      puts "No element found for: #{followers_el}"
+      return
+    end
+
     followers_count = followers_el.inner_text.gsub(/[,.]/,'').split(" ").find{|t| (t.try(:to_i) || 0) > 0}.try(:to_i) || 0
-    update!(snapshot: snapshot.merge({ DateTime.now.iso8601 => followers_count })) if followers_count > 0
+    update!(snapshot: snapshot.merge({ Date.today.iso8601 => followers_count })) if followers_count > 0
   end
 end
