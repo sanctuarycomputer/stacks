df9ddb56c27618d019e0c7114c7c87be0b804039
Josephine <josephine@sanctuary.computer>
2024-03-26 11:27:01 -0400

    feat: revert to non json api spec and parse strings to floats

--- app/controllers/api/profit_share_passes_controller.rb
+++ app/controllers/api/profit_share_passes_controller.rb
@@ -1,6 +1,6 @@
 class Api::ProfitSharePassesController < ActionController::Base
   def index
     result = ProfitSharePass.where.not(snapshot: nil)
-    render json: Api::ProfitSharePassSerializer.new(result)
+    render json: result, each_serializer: Api::ProfitSharePassSerializer
   end
 end
--- app/serializers/api/profit_share_pass_serializer.rb
+++ app/serializers/api/profit_share_pass_serializer.rb
@@ -1,43 +1,46 @@
-class Api::ProfitSharePassSerializer 
-  include JSONAPI::Serializer
+class Api::ProfitSharePassSerializer < ActiveModel::Serializer
   attributes :id, :total_psu_issued, :efficiency_cap_from_snapshot, 
   :desired_buffer_months, :gross_revenue, :gross_expenses, :gross_payroll, 
-  :projected_monthly_cost_of_doing_business, :fica_tax_rate, :internals_budget_multiplier
+  :projected_monthly_cost_of_doing_business, :fica_tax_rate, :internals_budget_multiplier, :year
+
+  def year
+    object.created_at.year
+  end
 
   def total_psu_issued
     object.total_psu_issued.round
   end
 
   def efficiency_cap_from_snapshot
-    object.efficiency_cap_from_snapshot
+    object.efficiency_cap_from_snapshot.to_f
   end
 
   def desired_buffer_months
-    object.desired_buffer_months
+    object.desired_buffer_months.to_f
   end
 
   def gross_revenue
-    object.gross_revenue
+    object.gross_revenue.to_f
   end
 
   def gross_expenses
-    object.gross_revenue
+    object.gross_expenses.to_f
   end
 
   def gross_payroll
-    object.gross_payroll
+    object.gross_payroll.to_f
   end
 
   def projected_monthly_cost_of_doing_business
-    object.projected_monthly_cost_of_doing_business
+    object.projected_monthly_cost_of_doing_business.to_f
   end
 
   def fica_tax_rate
-    object.fica_tax_rate
+    object.fica_tax_rate.to_f
   end
 
   def internals_budget_multiplier
-    object.internals_budget_multiplier
+    object.internals_budget_multiplier.to_f
   end
 
 end
