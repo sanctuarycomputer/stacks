760fae35e49bd148cdce798b3e436dd10a1fe5fa
Josephine <josephine@sanctuary.computer>
2024-03-27 16:31:23 -0400

    fix: avoid deprecation warning

--- app/controllers/api/profit_share_passes_controller.rb
+++ app/controllers/api/profit_share_passes_controller.rb
@@ -1,6 +1,6 @@
 class Api::ProfitSharePassesController < ActionController::Base
   def index
-    result = ProfitSharePass.finalized.order(:created_at)
+    result = ProfitSharePass.where.not(snapshot: nil).order(:created_at)
     render json: result, each_serializer: Api::ProfitSharePassSerializer
   end
 end
