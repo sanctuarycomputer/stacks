eb5e6ec7b9f777eb1baeea31486fc316464b2cc4
Josephine <josephine@sanctuary.computer>
2024-03-27 15:54:54 -0400

    fix: serializer handles nil snapshot

--- app/serializers/api/profit_share_pass_serializer.rb
+++ app/serializers/api/profit_share_pass_serializer.rb
@@ -12,35 +12,35 @@ class Api::ProfitSharePassSerializer < ActiveModel::Serializer
   :year
 
   def desired_buffer_months
-    object.snapshot.dig("inputs", "desired_buffer_months").to_f
+    object.snapshot&.dig("inputs", "desired_buffer_months").to_f
   end
   
   def efficiency_cap
-    object.snapshot.dig("inputs", "efficiency_cap").to_f
+    object.snapshot&.dig("inputs", "efficiency_cap").to_f
   end
   
   def fica_tax_rate
-    object.snapshot.dig("inputs", "fica_tax_rate").to_f
+    object.snapshot&.dig("inputs", "fica_tax_rate").to_f
   end
   
   def gross_expenses
-    object.snapshot.dig("inputs", "actuals", "gross_expenses").to_f
+    object.snapshot&.dig("inputs", "actuals", "gross_expenses").to_f
   end
   
   def gross_payroll
-    object.snapshot.dig("inputs", "actuals", "gross_payroll").to_f
+    object.snapshot&.dig("inputs", "actuals", "gross_payroll").to_f
   end
   
   def gross_revenue
-    object.snapshot.dig("inputs", "actuals", "gross_revenue").to_f
+    object.snapshot&.dig("inputs", "actuals", "gross_revenue").to_f
   end
   
   def internals_budget_multiplier
-    object.snapshot.dig("inputs", "internals_budget_multiplier").to_f
+    object.snapshot&.dig("inputs", "internals_budget_multiplier").to_f
   end
   
   def projected_monthly_cost_of_doing_business
-    object.snapshot.dig("inputs", "projected_monthly_cost_of_doing_business").to_f
+    object.snapshot&.dig("inputs", "projected_monthly_cost_of_doing_business").to_f
   end
   
   def total_psu_issued
