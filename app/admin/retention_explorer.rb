05d9edd3e1d08a71ba58cb1a7122808c24e6a8b1
Hugh Francis <hughfrancis89@gmail.com>
2024-04-18 14:15:17 -0400

    Fix ActiveAdmin::DatabaseHitDuringLoad

--- app/admin/retention_explorer.rb
+++ app/admin/retention_explorer.rb
@@ -1,10 +1,9 @@
 ActiveAdmin.register_page "Retention Explorer" do
   menu label: "Retention Explorer", parent: "Team"
 
-  admin_users_sorted_by_tenure_in_days =
-    Stacks::Team.admin_users_sorted_by_tenure_in_days
-
   content title: proc { I18n.t("active_admin.retention_explorer") } do
+    admin_users_sorted_by_tenure_in_days =
+      Stacks::Team.admin_users_sorted_by_tenure_in_days
     render(partial: "show", locals: {
       mean_tenure_in_days: Stacks::Team.mean_tenure_in_days(admin_users_sorted_by_tenure_in_days),
       admin_users_sorted_by_tenure_in_days: Stacks::Team.admin_users_sorted_by_tenure_in_days
