d921eb172c50903d635adbfad0781a089f624421
Hugh Francis <hughfrancis89@gmail.com>
2024-05-21 11:53:05 -0400

    Fix comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def prettify_datetime(datetime)
+    datetime ? datetime.strftime("%B %d, %Y") : "—"
+  end
 end
--- db/migrate/20240520174007_add_page_title_to_notion_pages.rb
+++ db/migrate/20240520174007_add_page_title_to_notion_pages.rb
@@ -2,11 +2,7 @@ class AddPageTitleToNotionPages < ActiveRecord::Migration[6.0]
   def change
     add_column :notion_pages, :page_title, :string, default: "", null: false
     NotionPage.all.each do |np|
-      begin
-        np.update!(page_title: np.page_title)
-      rescue => e
-        binding.pry
-      end
+      np.update!(page_title: (np.data.dig("properties", "Name", "title")[0] || {}).dig("plain_text") || "")
     end
   end
 end
