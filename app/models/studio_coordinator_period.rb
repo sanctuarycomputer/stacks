ca359f13907558d09f521dab7fa4273a9c1dcb5e
Hugh Francis <hugh@sanctuary.computer>
2024-06-10 11:40:18 -0400

    Add ActsAsPeriod to StudioCoordinatorPeriod

--- app/models/studio_coordinator_period.rb
+++ app/models/studio_coordinator_period.rb
@@ -1,15 +1,13 @@
 class StudioCoordinatorPeriod < ApplicationRecord
+  include ActsAsPeriod
+
   belongs_to :studio
   belongs_to :admin_user
 
   validates_presence_of :started_at
   validate :ended_at_before_started_at?
 
-  def ended_at_before_started_at?
-    if ended_at.present?
-      unless ended_at > started_at
-        errors.add(:started_at, "Must be before ended_at")
-      end
-    end
+  def sibling_periods
+    admin_user.studio_coordinator_periods
   end
 end
