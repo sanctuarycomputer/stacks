3e85730a74808f860cd37cac05397147b27e7e9b
Hugh Francis <hughfrancis89@gmail.com>
2024-07-29 15:43:55 -0700

    Fix tests

--- app/models/project_tracker_runn_project.rb
+++
@@ -1,7 +0,0 @@
-class ProjectTrackerRunnProject < ApplicationRecord
-  belongs_to :project_tracker
-  #belongs_to :runn_project, primary_key: :runn_id
-  belongs_to :runn_project, class_name: "RunnProject", foreign_key: "runn_project_id", primary_key: "runn_id"
-
-  validates :runn_project, uniqueness: true
-end
