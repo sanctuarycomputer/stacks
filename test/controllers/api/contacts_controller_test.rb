4e2c2e391fece43884172002d270e19cf925f585
Hugh Francis <hughfrancis89@gmail.com>
2025-05-26 21:59:19 -0600

    Fix

--- test/controllers/api/contacts_controller_test.rb
+++ test/controllers/api/contacts_controller_test.rb
@@ -7,32 +7,29 @@ class Api::ContactsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "it fails with bad email address" do
-    post "/api/contacts", {
+    post "/api/contacts",
       headers: { "X-Api-Key" => Stacks::Utils.config[:stacks][:private_api_key] },
       params: { email: "hugh@" },
       as: :json
-    }
     assert_response :unprocessable_entity
   end
 
   test "it works with a correct API key" do
     assert_difference("Contact.count", 1) do
-      post "/api/contacts", {
+      post "/api/contacts",
         headers: { "X-Api-Key" => Stacks::Utils.config[:stacks][:private_api_key] },
         params: { email: "hugh@sanctuary.computer" },
         as: :json
-      }
     end
 
     assert Contact.first.email, "hugh@sanctuary.computer"
     assert Contact.first.sources, []
 
     assert_difference("Contact.count", 0) do
-      post "/api/contacts", {
+      post "/api/contacts",
         headers: { "X-Api-Key" => Stacks::Utils.config[:stacks][:private_api_key] },
         params: { email: "hugh@sanctuary.computer", sources: ["substack"] },
         as: :json
-      }
     end
 
     assert Contact.first.email, "hugh@sanctuary.computer"
