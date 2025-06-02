b75002f00ab733dc4046061bd55612c761513efd
Hugh Francis <hughfrancis89@gmail.com>
2025-06-02 16:06:54 -0400

    Unsafe parsing

--- config/initializers/uri_escape_shim.rb
+++ config/initializers/uri_escape_shim.rb
@@ -1,9 +1,12 @@
-require 'cgi'
 
 unless URI.respond_to?(:escape)
   module URI
-    def self.escape(str)
-      CGI.escape(str.to_s)
+    def self.escape(str, unsafe = nil)
+      if unsafe
+        URI::DEFAULT_PARSER.escape(str.to_s, unsafe)
+      else
+        URI::DEFAULT_PARSER.escape(str.to_s)
+      end
     end
   end
 end
